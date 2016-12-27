a8e4796caeed0d96472940dc2e909407a2f48783
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-27 15:47:42 +0100

    Strong reference-cycle, no deinit possible

--- The_weakest_strongest_and_unowned_References.playground/Contents.swift
+++ The_weakest_strongest_and_unowned_References.playground/Contents.swift
@@ -1,3 +1,40 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+class Person {
+    
+    var workingOnMachine: Machine?
+    
+    init() {
+        NSLog("Person.init()")
+    }
+    
+    deinit {
+        NSLog("Person.deinit()")
+    }
+}
+
+class Machine {
+    
+    var machineOperator: Person?
+    
+    init() {
+        NSLog("Machine.init()")
+    }
+    
+    deinit {
+        NSLog("Machine.deinit()")
+    }
+    
+    func startWorking(withOperator: Person) {
+        machineOperator = withOperator
+    }
+}
+
+var machine: Machine?
+machine = Machine()
+var operatorPerson = Person()
+machine?.startWorking(withOperator: operatorPerson)
+operatorPerson.workingOnMachine = machine
+machine = nil
