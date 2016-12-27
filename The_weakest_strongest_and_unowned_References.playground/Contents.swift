e54278b1a3c76e73931970cd254d5fb1c9d633db
Stefan Mehnert <StefanMehnert@gmail.com>
2016-12-27 15:54:43 +0100

    Provocated strong reference cycle in a closure

--- The_weakest_strongest_and_unowned_References.playground/Contents.swift
+++ The_weakest_strongest_and_unowned_References.playground/Contents.swift
@@ -5,15 +5,25 @@ import UIKit
 
 class Machine {
     
-    init() {
+    let internalName: String
+    
+    init(internalName: String) {
         NSLog("Machine.init()")
+        
+        self.internalName = internalName
     }
     
     deinit {
         NSLog("Machine.deinit()")
     }
+    
+    lazy var externalName: () -> String = {
+        
+        return self.internalName
+    }
 }
 
 var machine: Machine?
-machine = Machine()
+machine = Machine(internalName: "CS 140.2")
+machine?.externalName()
 machine = nil
